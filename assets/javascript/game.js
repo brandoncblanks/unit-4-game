ddaf6a2de3686a953c919a3982fcf08ea8100027
Brandon Blanks <brandonblanks@brandons-mbp.attlocal.net>
2019-04-23 19:36:54 -0400

    added notes and js notes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,20 @@
 
+    //i know i am supposed to use these some how but not sure what to do here...
+    //need to make the crystals buttons, make them click, and then mnake them add numbers to the score,
+    //also need them crystal buttons to generate numbers when game starts
+    //need to have score add them up
+    //have the win and loses add up and total
+    //
 
 
+
+
+
+
+
+Math.random
+
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
